5bc522ff75634b92ed52f206a2b0f52cca0789ab
Saksham Kaushik <115393205+sakshampandit1@users.noreply.github.com>
2025-07-19 11:21:00 +0530

    completed exercise Using Fragments

---
+++ exercise_11/src/App.jsx
@@ -0,0 +1,20 @@
+// Coding Exercise 13: Using Fragments
+
+
+function Summary({ text }) {
+  return <>
+    <h1>Summary</h1>
+    <p>{text}</p>
+    </>
+}
+
+
+function App() {
+  return (
+    <div id="app" data-testid="app">
+      <Summary text="Fragments help you avoid unnecessary HTML elements." />
+    </div>
+  );
+}
+
+export default App;
